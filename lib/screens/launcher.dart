b3d391572b3af02407f78515e1634ebc6ab91960
DataBase18 <aramireza18@miumg.edu.gt>
2023-10-13 12:47:00 -0600

    first commit

---
+++ lib/screens/launcher.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class LauncherScreen extends StatelessWidget {
+  const LauncherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+    );
+  }
+}
